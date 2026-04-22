5ea2aa396f923944194e8fcf32060dc5a892cfa9
shubham12-prof <shubhsemwal12@gmail.com>
2026-04-22 17:34:00 +0530

    fix pooling

--- src/db.ts
+++ src/db.ts
@@ -4,8 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 const prisma = new PrismaClient({ adapter });
