920e255dd6abcbc05dab37ad16662201424670e1
shubham12-prof <shubhsemwal12@gmail.com>
2026-04-22 19:26:39 +0530

    git commit -m "use pooler for production"

--- src/db.ts
+++ src/db.ts
@@ -4,11 +4,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 const prisma = new PrismaClient({ adapter });
