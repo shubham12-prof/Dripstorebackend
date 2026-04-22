ffe888dca780db46cfe37dd835b7bc5e426f383e
shubham12-prof <shubhsemwal12@gmail.com>
2026-04-22 16:10:39 +0530

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "*",
-      "http://localhost:3000",
-      "https://dripstorefrontend.vercel.app/",
-    ],
-    credentials: true,
+    origin: "*",
+    credentials: false,
   }),
 );
 app.use(express.json());
